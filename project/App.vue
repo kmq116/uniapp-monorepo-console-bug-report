efc0c97c84f44723bc42425925e3e4033b086afb
飞天御剑流co <521luqiya@gmail.com>
2024-01-03 21:04:49 +0800

    feat: 添加 fn3 函数

--- project/App.vue
+++ project/App.vue
@@ -1,6 +1,9 @@
 <script>
 import { fn1, fn2 } from "app-handle-packet";
-
+export function fn3() {
+  console.log("fn3", "fn3 log2");
+  
+}
 export default {
   onLaunch: function () {
     console.log("fn1 appvue 调用");
@@ -8,6 +11,9 @@ export default {
 
     console.log("fn2 appvue 调用");
     fn2();
+
+    console.log("fn3 调用，结果正常打印");
+    fn3();
   },
   onShow: function () {},
   onHide: function () {},
